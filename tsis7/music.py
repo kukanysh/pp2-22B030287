e8e684612af288375ef27475c3dfc94acdf43230
kukanysh <260505kuka@gmail.com>
2023-02-21 00:12:25 +0600

    upd

--- tsis7/music.py
+++ tsis7/music.py
@@ -1,43 +1,49 @@
 import pygame
 import os
-
-_sound_library = {}
-def play_sound(path):
-  global _sound_library
-  sound = _sound_library.get(path)
-  if sound == None:
-    canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-    sound = pygame.mixer.Sound(canonicalized_path)
-    _sound_library[path] = sound
-  sound.play()
-
-_image_library = {}
-def get_image(path):
-    global _image_library
-    image = _image_library.get(path)
-    if image == None:
-        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-        image = pygame.image.load(canonicalized_path)
-        _image_library[path] = image
-    return image
+import time
 
 pygame.init()
-
+song_list = ["starboy.mp3", "partymonster.mp3", "falsealarm.mp3", "reminder.mp3", "rockin'.mp3", "secrets.mp3", "truecolors.mp3", "stargirl interlude.mp3", "sidewalks.mp3", "sixfeetunder.mp3", "lovetolay.mp3", "lonelynight.mp3", "attention.mp3", "ordinarylife.mp3", "nothingwithoutyou.mp3", "alliknow.mp3", "dieforyou.mp3", "ifeelitcoming.mp3"]
+song_names = ["Starboy", "Party Monster", "False Alarm", "Reminder", "Rockin'", "Secrets", "True Colors", "Stargirl Interlude", "Sidewalks", "Six Feet Under", "Love To Lay", "A Lonely Night", "Attention", "Ordinary Life", "Nothing Without You", "All I Know", "Die For You", "I Feel It Coming"]
 screen = pygame.display.set_mode((400, 600))
+pygame.display.set_caption('Starboy album player')
+
+font = pygame.font.SysFont('sf', 24)
+clock = pygame.time.Clock()
 running = True
-pygame.mixer.music.load('starboy.mp3')
+
+index = 0
+pygame.mixer.music.load(song_list[index])
+pygame.mixer.music.play()
+song_name = song_names[index]
+text = font.render("Currently playing:  " + song_name, True, ('black'))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_LEFT]: pygame.mixer.music.play()
-    if pressed[pygame.K_RIGHT]: pygame.mixer.music.stop()
+    if pressed[pygame.K_LEFT]:
+        index -= 1
+        pygame.mixer.music.load(song_list[index])
+        song_name = song_names[index]
+        text = font.render("Currently playing:  " + song_name, True, ('black'))
+        pygame.mixer.music.play()
+    if pressed[pygame.K_SPACE]: pygame.mixer.music.stop()
     if pressed[pygame.K_UP]: pygame.mixer.music.pause()
     if pressed[pygame.K_DOWN]: pygame.mixer.music.unpause()
-    screen.fill((255, 255, 255))
-    screen.blit(pygame.image.load('black.jpg'), (0, 0))
-    screen.blit(get_image('starboy.jpeg'), (100, 110)) #starboy
+    if pressed[pygame.K_RIGHT]: 
+        index += 1
+        pygame.mixer.music.load(song_list[index])
+        song_name = song_names[index]
+        text = font.render("Currently playing:  " + song_name, True, ('black'))
+        pygame.mixer.music.play()
+    if index == len(song_list) - 1:
+        index = -1
 
+
+    screen.fill((255, 255, 255))
+    screen.blit(pygame.image.load('backstar.jpg'), (0, 0))
+    screen.blit(text, (22, 440))
     pygame.display.flip()
+    clock.tick(5.5)
