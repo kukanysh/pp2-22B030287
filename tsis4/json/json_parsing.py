acd89901b3fa1dd67ee1cd7ecccc61e8dee8586b
kukanysh <260505kuka@gmail.com>
2023-02-21 00:40:40 +0600

    upd

--- tsis4/json/json_parsing.py
+++ tsis4/json/json_parsing.py
@@ -1,14 +1,17 @@
 import json
-with open("sample-data.json", "r") as read_file:
-    data = json.load(read_file)
-print(data)
-print("""Interface Status
-================================================================================""")
-print("""DN                                             Description          Speed                      MTU """)
-for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
-    if i == 'dn':
-        print(k, end="                          ")
-    if i == "speed":
-        print(k, end="                                         ")
-    if i == "mtu":
-        print(k, end="                   ")
+
+with open('sample-data.json') as f:
+    data = json.load(f)
+    
+print("Interface Status")
+print("=" * 80)
+print("{:50s}{:25s}{:10s}{:6s}".format("DN", "Description", "Speed", "MTU"))
+print("{:-<50}{:-<25}{:-<10}{:-<6}".format("", "", "", ""))
+
+for interface in data['imdata']:
+    dn = interface['l1PhysIf']['attributes']['dn']
+    description = interface['l1PhysIf']['attributes']['descr']
+    speed = interface['l1PhysIf']['attributes']['speed']
+    mtu = interface['l1PhysIf']['attributes']['mtu']
+    row = "{:50s}{:25s}{:10s}{:6s}".format(dn, description, speed, mtu)
+    print(row)
