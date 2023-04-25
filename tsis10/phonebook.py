bd03cd61be07be491e15aa8935bdbb35dd002726
kukanysh <260505kuka@gmail.com>
2023-04-25 23:34:06 +0600

    upd

--- tsis10/phonebook.py
+++ tsis10/phonebook.py
@@ -1,1 +1,24 @@
 import psycopg2
+
+print("===========================================")
+print("What type of action do you want to execute?")
+print("1) UPLOAD DATA FROM CSV FILE")
+print("2) UPLOAD DATA FROM CONSOLE")
+num = int(input())
+connection = psycopg2.connect(
+    host = "localhost",
+    database = "phonebook",
+    user = "postgres",
+    password = "password"
+)
+
+cursor = connection.cursor()
+
+# if num == 1:
+#     COPY users(username, first_name, last_name, phone_number) FROM 'path' WITH (FORMAT csv, HEADER);
+           
+# elif num == 2:
+
+connection.commit()
+cursor.close()
+connection.close()
---
+++ tsis10/phonebook.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(12) NOT NULL
+);
+
